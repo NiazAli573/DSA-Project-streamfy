0f5256d06d8191c0de9cebee4bb47252f1c4ae78
MuhammadMehdiRaza <93407225+MuhammadMehdiRaza@users.noreply.github.com>
2024-12-08 17:22:20 +0500

    Update 2023466.cpp
    HashMap implementation ,taking user person Data

--- 2023466.cpp
+++ 2023466.cpp
@@ -1,7 +1,27 @@
-#include<iostream>
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+#include <string>
 using namespace std;
-int main(){
 
-  cout<<"Project"<<endl;
-  
+struct UserProfile {
+    string password;
+    string email;
+    string userID;
+};
+
+int main() {
+    
+    unordered_map<string, UserProfile> userDB;
+    
+    userDB["creator"] = {"Mehdi2001", "muhammad@gmail.com","Mehdi"};
+
+    string viewerID = "user1";
+    userDB[viewerID] = {"Data", "data@gmail.com","Yay_User"};
+    
+    cout << "Creator Email: " << userDB["creator"].userID << endl;
+    
+    cout << "Viewer Email: " << userDB[viewerID].userID << endl;
+
+    return 0;
 }
